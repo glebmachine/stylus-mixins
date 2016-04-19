9ce8bb9fbc0749a9453bd41ff843365a41fb333e
Gleb Mikheev <mikheev@betaagency.ru>
2016-04-19 17:54:46 +0300

    readme file ...etc

---
+++ README.md
@@ -0,0 +1,2 @@
+
+## My own stylus mixins pack. Feel free to use or share with friends
--- functions/file-exists.js
+++ functions/file-exists.js
@@ -2,6 +2,12 @@ var stylus = require('stylus');
 
 module.exports = function() {
   return function(style) {
+
+    /**
+     * Check if file exists in file system
+     * @param  {string} relative file path
+     * @return {boolean} is file exists (`true/false`)
+     */
     style.define('file-exists', function(path) {
       return !!stylus.utils.lookup(path.string, this.paths);
     });
